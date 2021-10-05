ff1cd5d3a7ba2251abfa1bc7d238e3d1ca87e261
Jonathan Ogden <jozgd@umsystem.edu>
2021-10-04 20:53:02 -0500

    fixed SmartCard

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('SmartCard'),
+        title: const Text('SmartCart'),
       ),
       body: Center(
         child: Column(
